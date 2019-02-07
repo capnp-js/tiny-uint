3a937a5e38656781bf18709c2253533a2e914211
Tim Popham <popham@uw.edu>
2019-02-07 15:02:28 -0800

    Switch to Mocha and Istanbul

--- test/unit/u2.js
+++ test/unit/u2.js
@@ -1,41 +1,36 @@
 /* @flow */
 
-import test from "ava";
+import * as assert from "assert";
+import { describe, it } from "mocha";
 
 import { u2_mask } from "../../src/u2";
 
-test("`u2_mask` with value 0", t => {
-  t.plan(4);
+describe("u2_mask", function () {
+  it("masks 0 to 0", function () {
+    assert.equal(u2_mask(0x00, 0x00), 0x00);
+    assert.equal(u2_mask(0x00, 0x01), 0x00);
+    assert.equal(u2_mask(0x00, 0x02), 0x00);
+    assert.equal(u2_mask(0x00, 0x03), 0x00);
+  });
 
-  t.is(u2_mask(0x00, 0x00), 0x00);
-  t.is(u2_mask(0x00, 0x01), 0x00);
-  t.is(u2_mask(0x00, 0x02), 0x00);
-  t.is(u2_mask(0x00, 0x03), 0x00);
-});
+  it("masks 1 correctly", function () {
+    assert.equal(u2_mask(0x01, 0x00), 0x00);
+    assert.equal(u2_mask(0x01, 0x01), 0x01);
+    assert.equal(u2_mask(0x01, 0x02), 0x00);
+    assert.equal(u2_mask(0x01, 0x03), 0x01);
+  });
 
-test("`u2_mask` with value 1", t => {
-  t.plan(4);
+  it("masks 2 correctly", function () {
+    assert.equal(u2_mask(0x02, 0x00), 0x00);
+    assert.equal(u2_mask(0x02, 0x01), 0x00);
+    assert.equal(u2_mask(0x02, 0x02), 0x02);
+    assert.equal(u2_mask(0x02, 0x03), 0x02);
+  });
 
-  t.is(u2_mask(0x01, 0x00), 0x00);
-  t.is(u2_mask(0x01, 0x01), 0x01);
-  t.is(u2_mask(0x01, 0x02), 0x00);
-  t.is(u2_mask(0x01, 0x03), 0x01);
-});
-
-test("`u2_mask` with value 2", t => {
-  t.plan(4);
-
-  t.is(u2_mask(0x02, 0x00), 0x00);
-  t.is(u2_mask(0x02, 0x01), 0x00);
-  t.is(u2_mask(0x02, 0x02), 0x02);
-  t.is(u2_mask(0x02, 0x03), 0x02);
-});
-
-test("`u2_mask` with value 3", t => {
-  t.plan(4);
-
-  t.is(u2_mask(0x03, 0x00), 0x00);
-  t.is(u2_mask(0x03, 0x01), 0x01);
-  t.is(u2_mask(0x03, 0x02), 0x02);
-  t.is(u2_mask(0x03, 0x03), 0x03);
+  it("masks 3 correctly", function () {
+    assert.equal(u2_mask(0x03, 0x00), 0x00);
+    assert.equal(u2_mask(0x03, 0x01), 0x01);
+    assert.equal(u2_mask(0x03, 0x02), 0x02);
+    assert.equal(u2_mask(0x03, 0x03), 0x03);
+  });
 });
